25cbc29e1600ff81f7055037525da8ecf21cac9f
server tombak <69975139+ServerToMbak@users.noreply.github.com>
2023-11-02 17:13:49 +0300

    tests for ClubRepository and clubController with XUnit, FakeItEasy and FluentAssertion

---
+++ RunGroopWebAPP.Test/Controller/ClubControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Controllers;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebAPP.Test.Controller
+{
+    public class ClubControllerTests
+    {
+        private readonly IClubRepository _clubRepository;
+        private readonly IPhotoService _photoServices;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClubController _clubController;
+
+        public ClubControllerTests()
+        {
+            _clubRepository = A.Fake<IClubRepository>();
+            _photoServices = A.Fake<IPhotoService>();
+            _httpContextAccessor = A.Fake<IHttpContextAccessor>();
+
+            //SUT --
+            _clubController = new ClubController(_clubRepository, _photoServices, _httpContextAccessor);
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnSuccess()
+        {
+            //Arrange
+            var clubs = A.Fake<IEnumerable<Club>>();
+            A.CallTo(()=> _clubRepository.GetAll()).Returns(clubs);
+            //Act
+            var result = _clubController.Index();
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+
+        }
+        [Fact]
+        public void ClubController_Detail_ReturnSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var club = A.Fake<Club>();
+            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
+
+            //Act
+            var result = _clubController.Detail(id);
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+    }
+}
---
+++ RunGroopWebAPP.Test/Repository/ClubRepositoryTests.cs
@@ -0,0 +1,108 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Data.Enum;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+
+namespace RunGroopWebAPP.Test.Repository
+{
+    public class ClubRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var databaseContext = new ApplicationDbContext(options);
+
+            databaseContext.Database.EnsureCreated();
+            if(await databaseContext.Clubs.CountAsync()<0)
+            {
+                for(int i = 0; i < 10; i++)
+                {
+
+                databaseContext.Clubs.Add(new Club()
+                {
+                    Title = "Running Club 1",
+                    Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                    Description = "This is the description of the first club",
+                    ClubCategory = ClubCategory.City,
+                    Address = new Address()
+                    {
+                        Street = "123 Main St",
+                        City = "Michigan",
+                        State = "NC"
+                    }
+                });
+                await databaseContext.SaveChangesAsync();
+
+                }
+            }
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void ClubRepoistory_Add_ReturnsBool()
+        {
+
+            //Arrange
+            var club = new Club
+            {
+                Title = "Running Club 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first club",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Michigan",
+                    State = "NC"
+                }
+            };
+
+            var dbContext = await GetDbContext();
+            //dbContext.Clubs.AsNoTracking();
+            var clubRepository = new ClubRepoistory(dbContext);
+
+            //Act
+            var result = clubRepository.Add(club);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void ClubRepository_GetByIdAsync_returnsClub()
+        {
+            //Arrange
+            var id = 1;
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepoistory(dbContext);
+
+            //Act
+            var result = clubRepository.GetByIdAsync(id);
+
+            //Assert 
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<Club>>();
+
+        }
+
+        [Fact]
+        public async void ClubRepository_GetAll_ReturnsList()
+        {
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepoistory(dbContext);
+
+            //Act
+            var result = await clubRepository.GetAll();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<Club>>();
+        }
+    }
+}
